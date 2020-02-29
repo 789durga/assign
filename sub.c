f5f0400117953b2eb2b3276491ac070b8cfe94da
789durga <789durga@gmail.com>
2020-02-29 15:40:53 +0530

    rinoim

--- sub.c
+++ sub.c
@@ -1,11 +1,8 @@
 #include <stdio.h>
 int main() {    
 
-<<<<<<< HEAD
     int number1, num, diff
-=======
     int number1, number2, difference
->>>>>>> e6e488369e865e4c124fbcb94e1056ff318a4c4c
     
     printf("Enter two integers: ");
     scanf("%d %d", &number1, &number2);
