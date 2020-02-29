3f3a3db57d68e00775a9a2e08150e2d56569ca30
789durga <789durga@gmail.com>
2020-02-29 16:07:06 +0530

    dwedsasdwdew

--- sub.c
+++ sub.c
@@ -1,18 +1,7 @@
 #include <stdio.h>
 int main() {    
 
-<<<<<<< Updated upstream
-<<<<<<< Updated upstream
     int number1, num,f
-
-=======
-    int number1, num, diffdwejdnejkdnwjeewd
-    int number1, numifferencedsdsedew
->>>>>>> Stashed changes
-=======
-    int number1, num, diffdwejdnejkdnwjeewd
-    int number1, numifferencedsdsedew
->>>>>>> Stashed changes
     
     printf("Enter two integers: ");
     scanf("%d %d", &number1, &number2);
